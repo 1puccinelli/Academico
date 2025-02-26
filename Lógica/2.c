9c2b9a41d2726eaf03a18abf0a903470ee0556ba
Lucas V.S <lucas.viniciuss@outlook.com>
2025-02-26 08:46:08 -0300

    Soma
    Segundo trabalho de Lógica

---
+++ Lógica/2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, soma;
+    printf("Digite o primeiro numero ");
+    scanf("%d", &num1);
+    
+    printf("Digite o segundo numero ");
+    scanf("%d", &num2);
+
+    soma = num1 + num2;
+
+    printf("A soma do %d e %d e de %d\n", num1, num2, soma);
+
+    return 0;
+}
