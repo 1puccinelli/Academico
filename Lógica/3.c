c38d4affee929007556f025b8ecd7dc0d82ab5b4
Lucas V.S <lucas.viniciuss@outlook.com>
2025-02-26 09:15:35 -0300

    Área do raio
    Terceira tarefa de Lógica

---
+++ Lógica/3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float raio = 5;
+    float n = 3.14;
+    
+    float area = n * pow(raio, 2);
+
+    printf("%f", area);
+
+    return 0;
+}
